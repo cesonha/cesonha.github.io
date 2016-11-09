61f357cfbb309a340c65481083fe72739aa8d004
cecerhcp <cecerhcp0@hotmail.com>
2016-11-08 23:05:11 -0200

    fodase

--- script.js
+++ script.js
@@ -1,48 +1,17 @@
-function getTimeRemaining(endtime) {
-  var t = Date.parse(endtime) - Date.parse(new Date());
-  var seconds = Math.floor((t / 1000) % 60);
-  var minutes = Math.floor((t / 1000 / 60) % 60);
-  var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-  var days = Math.floor(t / (1000 * 60 * 60 * 24));
-  return {
-    'total': t,
-    'days': days,
-    'hours': hours,
-    'minutes': minutes,
-    'seconds': seconds
-  };
-}
 
-function initializeClock(id, endtime) {
-  var clock = document.getElementById(id);
+function vaimlk() {
 
-  try{
-  var daysSpan = clock.querySelector('.days');
-  var hoursSpan = clock.querySelector('.hours');
-  var minutesSpan = clock.querySelector('.minutes');
-  var secondsSpan = clock.querySelector('.seconds');
-  }
-  catch(err)
-  {
-   //alert(err.message);
-  }
-
-  function updateClock() {
-    var t = getTimeRemaining(endtime);
-
-    daysSpan.innerHTML = t.days;
-    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
-    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
-
-    if (t.total <= 0) {
-      clearInterval(timeinterval);
+  try {
+     var imagem = document.getElementById("foto");
+    imagem.src = imagem.src.replace("pq.jpg", "vtnc.png");
+    imagem.style.height = 100;
+    imagem.style.width = 887;
+    var audio = new Audio('tst.mp3');
+    audio.play();
     }
+    catch(err) {
+    document.getElementById("nois").value = err.message;
+    }
+
   }
 
-  updateClock();
-  var timeinterval = setInterval(updateClock, 1000);
-
-}
-var deadline = new Date("November 5, 2016 13:00:00");
-initializeClock("clockdiv", deadline);
